e9b3ad7518187add259c1a0edfbb3bbcc3d84eed
uesteibar <uesteibar@gmail.com>
2015-07-20 12:09:35 +0200

    create task model

--- 1-monday/taskly/db/schema.rb
+++ 1-monday/taskly/db/schema.rb
@@ -11,10 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720074653) do
+ActiveRecord::Schema.define(version: 20150720100808) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "tasks", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "completed_timestamp"
+    t.integer  "user_id"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
