b0fc01603b5eb4298fde9dc29eee1bbf08385b2a
uesteibar <uesteibar@gmail.com>
2015-07-20 10:57:45 +0200

    post route

--- 1-monday/taskly/app/controllers/users_controller.rb
+++ 1-monday/taskly/app/controllers/users_controller.rb
@@ -3,7 +3,24 @@ class UsersController < ApplicationController
     render json: User.all
   end
 
+  def create
+    user = User.new(user_params)
+    if user.valid?
+      user.save
+      render json: user
+    else
+      render status: 400, json: user.errors
+    end
+  end
+
   def show
-    render json: User.find(params[:id])
+    user = User.find(params[:id])
+    render json: user
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- 1-monday/taskly/config/routes.rb
+++ 1-monday/taskly/config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show]
+
+  resources :users, only: [:index, :create, :show]
+
 end
