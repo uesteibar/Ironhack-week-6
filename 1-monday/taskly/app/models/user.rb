6fa3fcd9b94f42d651bd1dc3aafcf346c0c0c8ac
uesteibar <uesteibar@gmail.com>
2015-07-20 12:20:32 +0200

    test and validate task model

--- 1-monday/taskly/app/models/user.rb
+++ 1-monday/taskly/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  has_many :tasks, dependent: :destroy
+
   validates_presence_of :name, :email
   validates_uniqueness_of :name, :email
 end
--- 1-monday/taskly/spec/models/task_spec.rb
+++ 1-monday/taskly/spec/models/task_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Task, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before(:each) do
+      @user = User.create(name: "superman", email: "superemail@example.com")
+    end
+
+    it 'should create a task when description and user_id are provided' do
+      task = @user.tasks.create(description: "save the world")
+      expect(task.id).not_to be_falsy
+    end
+
+    it 'should NOT create a task when description is missing' do
+      expect do
+        @user.tasks.create!
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
+
+  describe "owner user destruction" do
+    before(:each) do
+      @user = User.create(name: "superman", email: "superemail@example.com")
+      @user.tasks.create(description: "save the world")
+    end
+    it 'should delete all the tests when the owner user is deleted' do
+      task = @user.tasks.first
+      @user.destroy
+      expect do
+        task.reload
+      end.to raise_error ActiveRecord::RecordNotFound
+    end
+  end
 end
