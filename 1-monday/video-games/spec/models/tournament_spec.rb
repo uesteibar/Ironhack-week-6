fab8a56f3946549aee8e7724dcdf9cd8d68f3f86
uesteibar <uesteibar@gmail.com>
2015-07-20 16:58:00 +0200

    create and validate tournament model

--- 1-monday/video-games/spec/models/tournament_spec.rb
+++ 1-monday/video-games/spec/models/tournament_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Tournament, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'creation' do
+    it 'should create a tournament when a name is provided' do
+      tournament = Tournament.create(name: "super tournament")
+      expect(tournament.id).not_to be_falsy
+    end
+
+    it 'should NOT create a tournament when name is nil' do
+      tournament = Tournament.create(name: nil)
+      expect(tournament.id).to be_falsy
+    end
+
+    it 'should NOT create a tournament when name is empty' do
+      tournament = Tournament.create(name: "")
+      expect(tournament.id).to be_falsy
+    end
+  end
 end
