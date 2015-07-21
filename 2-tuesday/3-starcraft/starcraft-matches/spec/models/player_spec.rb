d2ba4c9a950c52ef7825bbcb9054ee7f71d61d5d
uesteibar <uesteibar@gmail.com>
2015-07-21 15:05:52 +0200

    create and validate player model

---
+++ 2-tuesday/3-starcraft/starcraft-matches/spec/models/player_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Player, type: :model do
+  describe "creation" do
+    before(:each) do
+      @player_params = {
+        name: "uesteibar"
+      }
+    end
+
+    it 'should create a player if name is provided' do
+      player = Player.create!(@player_params)
+      expect(player.id).not_to be_falsy
+    end
+
+    it 'should NOT create a player if name is empty' do
+      expect do
+        Player.create!(name: "")
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+
+    it 'should NOT create a player if name is nil' do
+      expect do
+        Player.create!(name: nil)
+      end.to raise_error ActiveRecord::RecordInvalid
+    end
+  end
+end
