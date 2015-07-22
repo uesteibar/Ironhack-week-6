240aae57b1694ef7022c4f678e9dbfc26e4d2e16
uesteibar <uesteibar@gmail.com>
2015-07-22 12:44:33 +0200

    test authorization

--- 3-wednesday/concert-network/spec/controllers/concerts_controller_spec.rb
+++ 3-wednesday/concert-network/spec/controllers/concerts_controller_spec.rb
@@ -62,31 +62,72 @@ RSpec.describe ConcertsController, type: :controller do
   end
 
   describe 'GET #new' do
-    it 'responds successfully with an HTTP 200 status code' do
-      get :new
-      expect(response).to be_success
-      expect(response.code.to_i).to eq(200)
+    context 'when user is logged in' do
+      it 'responds successfully with an HTTP 200 status code' do
+        get :new
+        expect(response).to be_success
+        expect(response.code.to_i).to eq(200)
+      end
+
+      it 'renders the new template' do
+        get :new
+        expect(response).to render_template('new')
+      end
     end
 
-    it 'renders the new template' do
-      get :new
-      expect(response).to render_template('new')
+    context 'when user is NOT logged in' do
+      before(:each) do
+        session.clear
+      end
+
+      it 'responds successfully with an HTTP 200 status code' do
+        get :new
+        expect(response).not_to be_success
+        expect(response.code.to_i).to eq(302)
+      end
+
+      it 'redirects to root' do
+        get :new
+        expect(response).to redirect_to("/")
+      end
     end
   end
 
   describe 'POST #create' do
-    it 'creates a concert' do
-      params = {
-        band: 'Jessie J',
-        venue: 'Parque Pedralbes',
-        city_id: City.create(name: "Barcelona").id,
-        price: 20,
-        date: 1.week.from_now,
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-      }
-      expect do
-        post :create, concert: params
-      end.to change(Concert, :count).by(1)
+    context 'when user is logged in' do
+      it 'creates a concert' do
+        params = {
+          band: 'Jessie J',
+          venue: 'Parque Pedralbes',
+          city_id: City.create(name: "Barcelona").id,
+          price: 20,
+          date: 1.week.from_now,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+        expect do
+          post :create, concert: params
+        end.to change(Concert, :count).by(1)
+      end
+    end
+
+    context 'when user is not logged in' do
+      before(:each) do
+        session.clear
+      end
+      
+      it 'creates a concert' do
+        params = {
+          band: 'Jessie J',
+          venue: 'Parque Pedralbes',
+          city_id: City.create(name: "Barcelona").id,
+          price: 20,
+          date: 1.week.from_now,
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+        }
+        expect do
+          post :create, concert: params
+        end.to change(Concert, :count).by(0)
+      end
     end
   end
 end
