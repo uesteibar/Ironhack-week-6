2a2330679ecd23b72529012888ddcf04f3da123b
uesteibar <uesteibar@gmail.com>
2015-07-22 15:19:57 +0200

    implement devise

--- 3-wednesday/3-devise-exercise/devise-auth/config/routes.rb
+++ 3-wednesday/3-devise-exercise/devise-auth/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,  path: "",
+                      path_names: {sign_in: "login", sign_up: "signup"}
 
   root to: "home#index"
-  
+
 end
