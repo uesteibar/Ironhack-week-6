29779f94a11da81f1928138dfca0b6251fb925c4
uesteibar <uesteibar@gmail.com>
2015-07-22 11:48:00 +0200

    refactor and test user auth

--- 3-wednesday/concert-network/spec/controllers/sessions_controller_spec.rb
+++ 3-wednesday/concert-network/spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
+  describe 'POST #create' do
+    before(:each) do
+      @user = User.create(
+        username: "uesteibar",
+        email: "uesteibar@email.com",
+        password: "1234567890",
+        password_confirmation: "1234567890"
+      )
+    end
 
+    it 'should set a session cookie' do
+      post :create, email: @user.email, password: @user.password
+      expect(session[:user_id]).not_to be_falsy
+    end
+  end
+
+  describe 'POST #create' do
+    before(:each) do
+      @user = User.create(
+        username: "uesteibar",
+        email: "uesteibar@email.com",
+        password: "1234567890",
+        password_confirmation: "1234567890"
+      )
+      post :create, email: @user.email, password: @user.password
+    end
+
+    it 'should clear the session cookie' do
+      get :destroy
+      expect(session[:user_id]).to be_falsy
+    end
+  end
 end
